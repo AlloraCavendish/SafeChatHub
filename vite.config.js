9a5ae7e9dedffb64a1de92b3cdafd1bf24011582
Aiman Haziq <153014908+AlloraCavendish@users.noreply.github.com>
2024-12-04 18:44:19 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'client/build', // Ensure this is the correct directory
+  },
+  
   plugins: [react()],
   optimizeDeps: {
     include: ['qrcode.react']
